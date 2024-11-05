d772e6d9267024b2e50752da08c67e751705036f
zunguyen1505@gmail.com <zunguyen1505@gmail.com>
2024-11-05 22:42:09 +0700

    Vu Nguyen update gửi mail khi user cập nhật thông tin ở state 1 của consign user có thể update thông tin ký gửi

--- Backend/src/services/kois.services.js
+++ Backend/src/services/kois.services.js
@@ -14,7 +14,86 @@ class KoisService {
     return result.CategoryName
   }
 
-  async generateKoiRequestEmail(userResult, koiResult, consignResult) {
+  async generateConsignUpdateInformation(userResult, koiResult, consignResult, titleEmail) {
+    const getField = (field, fieldName) => (field ? field : `<span style="color: tomato;">chưa có ${fieldName}</span>`)
+
+    const nameCategory = await this.getNameCategoryByID(koiResult.CategoryID)
+
+    const getStateDescription = (state) => {
+      switch (state) {
+        case 1:
+          return '<span style="color: orange;">Yêu cầu ký gửi</span>'
+        case 2:
+          return '<span style="color: blue;">Đang kiểm tra Koi</span>'
+        case 3:
+          return '<span style="color: purple;">Đang thương thảo hợp đồng</span>'
+        case 4:
+          return '<span style="color: green;">Đang tìm người mua</span>'
+        case 5:
+          return '<span style="color: gold;">Đã bán thành công</span>'
+        case -1:
+          return '<span style="color: red;">Đã hủy</span>'
+        default:
+          return '<span style="color: gray;">Trạng thái không xác định</span>'
+      }
+    }
+
+    return `
+      <div style="font-family: Arial, sans-serif; line-height: 1.8; color: #333;  font-size: 14px;">
+        <div style="max-width: 1000px; margin: 0 auto; padding: 40px; border: 1px solid #ddd; border-radius: 20px; background-color: #f9f9f9;">
+          <h2 style="text-align: center; color: #4CAF50;">${titleEmail}</h2>
+          <p>Xin chào,</p>
+          <p>Dưới đây là thông tin chi tiết về việc ${titleEmail}:</p>
+          <div style="display: flex; justify-content: space-between; border: 1px solid #ddd; padding: 10px; border-radius: 10px; background-color: #fff;">
+            <div style="flex: 1; margin-right: 20px;">
+              <h3 style="text-align: center; color: #4CAF50;  font-size: 18px;">1. Thông Tin Liên Hệ</h3>
+              <ul>
+                <li><strong>Email:</strong> ${getField(userResult.email, 'email')}</li>
+                <li><strong>Tên:</strong> ${getField(userResult.name, 'tên')}</li>
+                <li><strong>Địa chỉ:</strong> ${getField(userResult.address, 'địa chỉ')}</li>
+                <li><strong>Số điện thoại:</strong> ${getField(userResult.phone_number, 'số điện thoại')}</li>
+              </ul>
+              <h3 style="text-align: center; color: #4CAF50; font-size: 18px;">3. Thông Tin Về Đơn Ký Gửi</h3>
+              <ul>
+                <li><strong>Ngày Koi được nhận tại cửa hàng:</strong> ${getField(consignResult.ShippedDate, 'ngày Koi được nhận tại cửa hàng')}</li>
+                <li><strong>Ngày nhận lại Koi:</strong> ${getField(consignResult.ReceiptDate, 'ngày nhận lại Koi')}</li>
+                <li><strong>Vị trí chăm sóc:</strong> ${getField(consignResult.PositionCare, 'vị trí chăm sóc')}</li>
+                <li><strong>Phương thức kí gửi:</strong> ${getField(consignResult.Method, 'phương thức kí gửi')}</li>
+                <li><strong>Chi tiết về đơn ký gửi:</strong> ${getField(consignResult.Detail, 'chi tiết')}</li>
+                <li><strong>Trạng thái:</strong> ${getStateDescription(consignResult.State)}</li>
+                <li><strong>Hoa hồng IKOI FARM nhận được cho đơn hàng:</strong> <span style="color: blue;">${getField(consignResult.Commission, 'hoa hồng')}</span></li>
+                <li><strong>Tổng tiền mà khách hàng sẽ nhận được cho đơn ký gửi:</strong> <span style="color: blue;">${getField(consignResult.TotalPrice, 'tổng tiền')}</span></li>
+              </ul>
+            </div>
+           <div style="flex: 1; margin-left: 20px;">
+              <h3 style="text-align: center; color: #4CAF50; font-size: 18px;">2. Thông Tin Cá Koi Ký Gửi</h3>
+              <ul>
+                <li><strong>Loài Koi:</strong> ${getField(nameCategory, 'loài Koi')}</li>
+                <li><strong>Tên Koi:</strong> ${getField(koiResult.KoiName, 'tên Koi')}</li>
+                <li><strong>Tuổi:</strong> ${getField(koiResult.Age, 'tuổi Koi')}</li>
+                <li><strong>Xuất xứ:</strong> ${getField(koiResult.Origin, 'xuất xứ Koi')}</li>
+                <li><strong>Giới tính:</strong> ${getField(koiResult.Gender, 'giới tính Koi')}</li>
+                <li><strong>Kích thước:</strong> ${getField(koiResult.Size, 'kích thước Koi')}</li>
+                <li><strong>Giống:</strong> ${getField(koiResult.Breed, 'giống Koi')}</li>
+                <li><strong>Mô tả:</strong> ${getField(koiResult.Description, 'mô tả Koi')}</li>
+                <li><strong>Lượng thức ăn hàng ngày:</strong> ${getField(koiResult.DailyFoodAmount, 'lượng thức ăn hàng ngày Koi')}</li>
+                <li><strong>Tỷ lệ lọc:</strong> ${getField(koiResult.FilteringRatio, 'tỷ lệ lọc Koi')}</li>
+                <li><strong>ID chứng nhận:</strong> ${getField(koiResult.CertificateID, 'ID chứng nhận Koi')}</li>
+                <li><strong>Giá:</strong> <span style="color: blue;">${getField(koiResult.Price, 'giá Koi')}</span></li>
+                <li><strong>Hình ảnh:</strong> ${getField(koiResult.Image, 'hình ảnh Koi')}</li>
+                <li><strong>Video:</strong> ${getField(koiResult.Video, 'video Koi')}</li>
+              </ul>
+            </div>
+          </div>
+          <p>Chúng tôi rất vui mừng chào đón bạn. Nếu bạn có bất kỳ câu hỏi nào, vui lòng liên hệ với chúng tôi.</p>
+          <p>Trân trọng,</p>
+          <p>Đội ngũ IKOI FARM</p>
+        </div>
+      </div>
+    `
+  }
+
+  async generateKoiRequestEmail(userResult, koiResult, consignResult, titleEmail) {
     const getField = (field, fieldName) => (field ? field : `<span style="color: tomato;">chưa có ${fieldName}</span>`)
 
     const nameCategory = await this.getNameCategoryByID(koiResult.CategoryID)
@@ -43,9 +122,9 @@ class KoisService {
     return `
       <div style="font-family: Arial, sans-serif; line-height: 1.8; color: #333;  font-size: 14px;">
         <div style="max-width: 1000px; margin: 0 auto; padding: 40px; border: 1px solid #ddd; border-radius: 20px; background-color: #f9f9f9;">
-          <h2 style="text-align: center; color: #4CAF50;">Yêu Cầu Kí Gửi Koi Tại IKOI FARM</h2>
+          <h2 style="text-align: center; color: #4CAF50;">${titleEmail}</h2>
           <p>Xin chào,</p>
-          <p>Dưới đây là thông tin chi tiết về yêu cầu ký gửi Koi của bạn:</p>
+          <p>Dưới đây là thông tin chi tiết về việc ${titleEmail}:</p>
           <p><a href="${confirmURL}" style="display: inline-block; padding: 10px 20px; font-size: 16px; color: #fff; background-color: #4CAF50; text-align: center; text-decoration: none; border-radius: 5px;">Xác nhận thông tin</a></p>
           <div style="display: flex; justify-content: space-between; border: 1px solid #ddd; padding: 10px; border-radius: 10px; background-color: #fff;">
             <div style="flex: 1; margin-right: 20px;">
@@ -345,7 +424,8 @@ class KoisService {
 
     // Cấu hình và gửi email
     // const verifyURL = `http://localhost:${process.env.PORT}/users/verify-email?email_verify_token=${email_verify_token}` // Đường dẫn xác nhận email
-    const emailContent = await this.generateKoiRequestEmail(userPayload, koiPayload, consignPayload)
+    const titleEmail = 'Yêu Cầu Kí Gửi Koi Tại IKOI FARM'
+    const emailContent = await this.generateKoiRequestEmail(userPayload, koiPayload, consignPayload, titleEmail)
 
     let mailOptions = {
       from: process.env.EMAIL_APP, // Thay thế bằng email của bạn
