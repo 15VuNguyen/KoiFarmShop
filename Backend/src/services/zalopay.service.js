eaa66bfee89d1ae3432248c5ac9e3d89745b4493
vantruong <phantryong@gmail.com>
2024-11-11 20:51:37 +0700

    V Truong update callbacl

--- Backend/src/services/zalopay.service.js
+++ Backend/src/services/zalopay.service.js
@@ -33,7 +33,7 @@ const zaloPayment = async (req, res) => {
     amount: req.body.total,
     description: `KOI Shop - Payment for the order + ${Math.floor(100000 + Math.random() * 900000)}`,
     bank_code: '',
-    callback_url: 'https://e749-2402-800-63a9-9a91-7579-f2e-c7c7-4f2a.ngrok-free.app/payment/callback'
+    callback_url: 'https://2dc4-171-247-178-74.ngrok-free.app/payment/callback'
   }
 
   // Tạo MAC cho yêu cầu
