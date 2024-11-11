e70de3aaf453640d15b3592e6ecc15cbd441c772
zunguyen1505@gmail.com <zunguyen1505@gmail.com>
2024-11-11 16:21:44 +0700

    Vu Nguyen sửa lại date cho đúng ngày và giờ của Việt Nam khi tạo tài khoản và update profile

--- Backend/src/models/schemas/User.schema.js
+++ Backend/src/models/schemas/User.schema.js
@@ -18,7 +18,11 @@ export default class UserSchema {
   address = ''
   phone_number = ''
   constructor(user) {
-    const date = new Date() //tạo này cho ngày created_at updated_at bằng nhau
+    const currentDate = new Date()
+    const vietnamTimezoneOffset = 7 * 60 // UTC+7 in minutes
+    const localTime = new Date(currentDate.getTime() + vietnamTimezoneOffset * 60 * 1000)
+
+    // const date = localTime.toISOString().replace('Z', '+07:00')
     this._id = user?._id ?? new ObjectId() // tự tạo id
     this.email = user.email
     this.password = user.password
@@ -28,8 +32,8 @@ export default class UserSchema {
     this.website = user.website || ''
     this.username = user.username || ''
     this.picture = user.picture || ''
-    this.created_at = user.created_at || date
-    this.updated_at = user.updated_at || date
+    this.created_at = localTime || user.created_at
+    this.updated_at = localTime || user.updated_at
     this.roleid = 1
     this.name = user.name || '' // nếu người dùng tạo mà k truyền ta sẽ để rỗng
     this.address = user.address || ''
