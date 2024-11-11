3909676a3a62dc41b4949bd1afb4f7093e56baed
vantruong <phantryong@gmail.com>
2024-11-11 21:29:06 +0700

    change callback URL

--- Backend/src/services/zalopay.service.js
+++ Backend/src/services/zalopay.service.js
@@ -33,7 +33,7 @@ const zaloPayment = async (req, res) => {
     amount: req.body.total,
     description: `KOI Shop - Payment for the order + ${Math.floor(100000 + Math.random() * 900000)}`,
     bank_code: '',
-    callback_url: 'https://2dc4-171-247-178-74.ngrok-free.app/payment/callback'
+    callback_url: 'https://koi-farm-shop.onrender.com/payment/callback'
   }
 
   // Tạo MAC cho yêu cầu
